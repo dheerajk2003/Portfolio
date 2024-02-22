bcc2e1f16fadb48d1a0b6121e1bdb37f3b2d00cd
Dheeraj <dheerajkhatri@gmail.com>
2024-02-22 18:33:07 +0530

    added tech stack icons in projects

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -1,14 +1,14 @@
 export function Nav(){
     return (
-        <div className="w-full max-w-screen h-1/12 absolute top-0 left-0 pt-5 px-3 flex justify-between items-center"> 
-            <h1 className="text-xl font-semibold border border-1 border-white p-1 px-4 rounded-md">Portfolio</h1>
-            <div className="w-2/5 h-full flex justify-around items-center border border-1 border-white p-2 rounded-md">
-                <a className="text-sm font-light" href="#home">Home</a>
-                <a className="text-sm font-light" href="#projects">Projects</a>
-                <a className="text-sm font-light" href="#skills">Skills</a>
-                <a className="text-sm font-light" href="#links">Links</a>
+        <div className="w-full max-w-screen absolute top-0 left-0 pt-5 px-3 flex justify-between items-center"> 
+            <h1 className="text-2xl font-bold border border-1 border-white p-2 px-5 rounded-md">Portfolio</h1>
+            <div className="w-2/5 h-full flex justify-around items-center border border-1 border-white p-3 rounded-md">
+                <a className="text-base font-light" href="#home">Home</a>
+                <a className="text-base font-light" href="#projects">Projects</a>
+                <a className="text-base font-light" href="#skills">Skills</a>
+                <a className="text-base font-light" href="#links">Links</a>
             </div>
-            <a className="text-sm font-medium border border-1 border-white p-2 px-4 rounded-md" href="dheerajkhatri2003@gmail.com">Contact</a>
+            <a className="text-base font-medium border border-1 border-white p-3 px-5 rounded-md" href="dheerajkhatri2003@gmail.com">Contact</a>
         </div>
     )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,28 +1,54 @@
-export function Projects(){
-    return(
+export function Projects() {
+    return (
         <div id="projects" className="w-full h-screen pt-14">
             <h2 className="w-32 mx-auto absolute -translate-x-1/2 left-1/2 text-4xl font-base border-b border-white border-1 ">Projects</h2>
             <div className="w-full h-full p-20 flex justify-around items-center">
                 <div className="w-1/3 h-4/5 m-5 border border-1 border-white rounded-2xl overflow-hidden">
-                    <img className="w-full grayscale" src="./recipe.jpg" alt="" />
-                    <div className="px-5">
-                        <h3 className="text-2xl font-medium my-3">Travelsy</h3>
-                        <p>Travelsy is a site where one can share and explore traveling places.</p>
+                    <img className="w-full content" src="./recipe.jpg" alt="" />
+                    <div className="h-1/2 px-5 flex flex-col justify-between">
+                        <div>
+                            <h3 className="text-2xl font-medium my-3">Travelsy</h3>
+                            <p className="inline" >Travelsy is a site where one can share and explore traveling places.</p>
+                            <a href="https://github.com/dheerajk2003/Travelsy" className="ml-1 inline text-blue-300">More</a>
+                        </div>
+                        <div className="h-10 flex flex-row justify-around">
+                            <div className="w-10 h-10"><img src="./svgs/c-sharp.svg"/></div>
+                            <div className="w-10 h-10"><img src="./svgs/asp.svg"/></div>
+                            <div className="w-10 h-10"><img src="./svgs/html.svg"/></div>
+                            <div className="w-10 h-10"><img src="./svgs/sql.svg"/></div>
+                        </div>
                     </div>
                 </div>
                 <div className="w-1/3 h-4/5 m-5 border border-1 border-white rounded-2xl overflow-hidden">
-                    <img className="w-full grayscale" src="./kmm.jpg" alt="" />
-                    <div className="px-5">
-                        <h3 className="text-2xl font-medium my-3">KMM</h3>
-                        <p>Kmm is a database of candidate who are looking for a special person in their life.</p>
+                    <img className="w-full content" src="./kmm.jpg" alt="" />
+                    <div className="h-1/2 px-5 flex flex-col justify-between">
+                        <div>
+                            <h3 className="text-2xl font-medium my-3">KMM</h3>
+                            <p className="inline" >Kmm is a database of candidate who are looking for a special person in their life.</p>
+                            <a href="https://github.com/dheerajk2003/KMM" className="ml-1 inline text-blue-300">More</a>
+                        </div>
+                        <div className="h-10 flex flex-row justify-around">
+                            <div className="w-10 h-10"><img src="./svgs/node.svg"/></div>
+                            <div className="w-10 h-10"><img src="./svgs/react.svg"/></div>
+                            <div className="w-10 h-10"><img src="./svgs/tailwind.svg"/></div>
+                            <div className="w-10 h-10"><img src="./svgs/mysql.svg"/></div>
+                        </div>
                     </div>
                 </div>
                 <div className="w-1/3 h-4/5 m-5 border border-1 border-white rounded-2xl overflow-hidden">
-                    <img className="w-full grayscale" src="./recipe.jpg" alt="" />
-                    <div className="px-5">
-                        <h3 className="text-2xl font-medium my-3">Recipied</h3>
-                        <p>It is a site where you can search and find tasty recipes.</p>
-                        <button className="bg-slate-500 p-3">hello</button>
+                    <img className="w-full content" src="./recipe.jpg" alt="" />
+                    <div className="h-1/2 px-5 flex flex-col justify-between">
+                        <div>
+                            <h3 className="text-2xl font-medium my-3">Recipied</h3>
+                            <p className="inline">It is a site where you can search and find tasty recipes.</p>
+                            <a href="https://github.com/dheerajk2003/Recipe-Website" className="ml-1 inline text-blue-300">More</a>
+                        </div>
+                        <div className="h-10 flex flex-row justify-around">
+                            <div className="w-10 h-10"><img src="./svgs/react.svg"/></div>
+                            <div className="w-10 h-10"><img src="./svgs/html.svg"/></div>
+                            <div className="w-10 h-10"><img src="./svgs/css.svg"/></div>
+                            <div className="w-10 h-10"><img src="./svgs/js.svg"/></div>
+                        </div>
                     </div>
                 </div>
             </div>
