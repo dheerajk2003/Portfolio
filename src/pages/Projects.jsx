1a805446dedae520969a3be4bdf74afe6b7a242e
Dheeraj Khatri <96782450+dheerajk2003@users.noreply.github.com>
2024-04-05 13:23:05 +0530

    added new project(dontcol) remove old(recipied)

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -36,18 +36,18 @@ export function Projects() {
                     </div>
                 </div>
                 <div className="w-full md:w-1/3 h-5/6 border border-1 border-white rounded-2xl overflow-hidden transition-transform card hover:shadow-md hover:shadow-[#7F27FF]">
-                    <img className="hidden md:block w-full" src="./recipe.jpg" alt="" />
+                    <img className="hidden md:block w-full" src="./dontcol.png" alt="" />
                     <div className="h-full md:h-1/2 px-5 flex flex-col justify-between gap-5">
                         <div>
-                            <h3 className="text-2xl font-medium my-3">Recipied</h3>
-                            <p className="inline">It is a site where you can search and find tasty recipes.</p>
-                            <a href="https://github.com/dheerajk2003/Recipe-Website" className="ml-1 inline text-blue-300">More</a>
+                            <h3 className="text-2xl font-medium my-3">Don't Collide</h3>
+                            <p className="inline">It is a online 2d multiplayer shooting game made with Unity, NodeJs and WebSockets.</p>
+                            <a href="https://github.com/dheerajk2003/DontCollide.git" className="ml-1 inline text-blue-300">More</a>
                         </div>
                         <div className="mb-4 md:mb-1 lg:mb-14 h-10 flex flex-row justify-around">
-                            <div className="w-10 h-10 lg:w-14 lg:h-14"><img className=" " src="./svgs/react.svg"/></div>
-                            <div className="w-10 h-10 lg:w-14 lg:h-14"><img className=" " src="./svgs/html.svg"/></div>
-                            <div className="w-10 h-10 lg:w-14 lg:h-14"><img className=" " src="./svgs/css.svg"/></div>
-                            <div className="w-10 h-10 lg:w-14 lg:h-14"><img className=" " src="./svgs/js.svg"/></div>
+                            <div className="w-10 h-10 lg:w-14 lg:h-14"><img className=" " src="./svgs/unity.svg"/></div>
+                            <div className="w-10 h-10 lg:w-14 lg:h-14"><img className=" " src="./svgs/node.svg"/></div>
+                            <div className="w-10 h-10 lg:w-14 lg:h-14"><img className=" " src="./svgs/websocket.svg"/></div>
+                            <div className="w-10 h-10 lg:w-14 lg:h-14"><img className=" " src="./svgs/c-sharp.svg"/></div>
                         </div>
                     </div>
                 </div>
